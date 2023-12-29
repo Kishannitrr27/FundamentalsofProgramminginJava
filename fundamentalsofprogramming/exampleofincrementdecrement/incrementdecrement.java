b527c3c6970169ca78d6ffeb173228d32044d9ea
Kishannitrr27 <155138812+Kishannitrr27@users.noreply.github.com>
2023-12-29 13:32:43 +0530

    Add files via upload

---
+++ fundamentalsofprogramming/exampleofincrementdecrement/incrementdecrement.java
@@ -0,0 +1,12 @@
+public class incrementdecrement {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = ++a;
+        System.out.println(a);
+
+        int c = 10;
+        int d= --c;
+        System.out.println(d);
+
+    }
+}
